b50aa42e08b5cd3e1a9e40b74dc64917608924d7
syed suhail <69636770+Suhail72@users.noreply.github.com>
2024-04-29 09:51:33 +0000

    datatype

---
+++ .devcontainer/01_basics/datatype.js
@@ -0,0 +1,4 @@
+// "use strict" treat all js code as newer version
+let name = "suhail";
+let age = 25;
+let isLoggedin = false;
--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,6 @@
 console.log("suhail");
+const accountId = 2567;
+let accountEmail = "jupiter@gmail.com";
+var accountPassword = "12456";
+accountCity = "jaipur";
+console.table([accountId, accountEmail, accountPassword, accountCity]);
