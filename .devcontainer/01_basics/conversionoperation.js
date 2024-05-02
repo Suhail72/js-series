d0ebf83bf859bcbac4a35ca46a5345bb65f670d1
syed suhail <69636770+Suhail72@users.noreply.github.com>
2024-05-02 05:47:42 +0000

    conversion operations

---
+++ .devcontainer/01_basics/conversionoperation.js
@@ -0,0 +1,39 @@
+/*let score = 33;
+console.log(typeof score);*/
+/*let score = "33";
+            "33abc"
+            null
+            undefined
+            true/false
+let valueInnumber = Number(score);
+console.log(typeof valueInnumber);
+console.log(valueInnumber);*/
+/*let isLoggedin = 1;
+let booleanIsloggedIn = Boolean(isLoggedin);
+console.log(booleanIsloggedIn);
+let somenumber = 33;
+let stringNumber = String(somenumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);*/
+
+//********** opeartions
+/*let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = "syed ";
+let str2 = "suhail";
+let str3 = str1 +  str2;
+console.log(str3);*/
+
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2);//122
+console.log(1 + 2 + "2");//32
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
