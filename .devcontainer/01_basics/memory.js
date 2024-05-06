aa42dd78a772ef30b583f71507c50c616c171bf3
syed suhail <69636770+Suhail72@users.noreply.github.com>
2024-05-06 06:02:09 +0000

    stack and heaop memory

---
+++ .devcontainer/01_basics/memory.js
@@ -0,0 +1,14 @@
+/*let myYoutubename = "hiteshChoudary";
+let anotherName = myYoutubename;
+anotherName = "chaiaurcode";
+console.log(myYoutubename);
+console.log(anotherName);*/
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne;
+userTwo.email = "suhail@google.com";
+console.log(userOne.email);
+console.log(userTwo.email);
