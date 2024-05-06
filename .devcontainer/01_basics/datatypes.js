a422e372a0ce1c87a291f56a783c2700f9c1074a
syed suhail <69636770+Suhail72@users.noreply.github.com>
2024-05-06 05:52:32 +0000

    reference datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,14 @@
+//array
+const heroes = ["Shaktiman","nagraj","doga"];
+
+//objects
+let myobj = {
+    name : "suhail",
+    age : 22,
+}
+
+//function
+const myFunction = function(){
+    console.log("hello world");
+}
+console.log(typeof heroes);
