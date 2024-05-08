0d0fec5f6ce4128b50de3dab9d7df862c492ee22
syed suhail <69636770+Suhail72@users.noreply.github.com>
2024-05-08 07:53:07 +0000

    date and time

---
+++ .devcontainer/01_basics/date.js
@@ -0,0 +1,8 @@
+/*let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toJSON());
+console.log(myDate.getHours());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());*/
+
