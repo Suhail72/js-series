bbd4ca0ce37265f3a2661b3e309462c5ea6d0cde
syed suhail <69636770+Suhail72@users.noreply.github.com>
2024-05-08 07:17:07 +0000

    string methods

---
+++ .devcontainer/01_basics/string.js
@@ -0,0 +1,27 @@
+/*const name = "Suhail";
+const repoCount = 50;
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);*/
+
+const gameName = new String('Suhail')
+/*console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.indexOf('h'));
+console.log(gameName.charAt(2));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+const anotherString = gameName.slice(-6,4);
+console.log(anotherString);
+
+const newStringOne = "  Suhail  ";
+console.log(newStringOne);
+console.log(newStringOne.trim());*/
+
+const url = "https://suhail.com/suhail%20mehadi";
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('suhail'));
+console.log(url.includes('dinesh'));
+
