168051deadaa9ef07b5caffeddd2d74a163dc86a
syed suhail <69636770+Suhail72@users.noreply.github.com>
2024-05-08 07:45:16 +0000

    number and maths

---
+++ .devcontainer/01_basics/math.js
@@ -0,0 +1,28 @@
+/*const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966;
+console.log(otherNumber.toPrecision(3));
+
+const hundreads = 100000000;
+console.log(hundreads.toLocaleString('en-IN'));
+
+ console.log(Math);
+ console.log(Math.abs(-7));
+ console.log(Math.round(4.7));
+ console.log(Math.ceil(3.3));
+ console.log(Math.floor(4.9));
+ console.log(Math.min(4,32,1,25));
+
+ console.log(Math.random());
+ console.log(Math.random()*10);
+ console.log((Math.random()*10)+1);
+ console.log(Math.floor(Math.random()*10)+1);*/
+
+
+ const min = 10;
+ const max = 20;
+ console.log(Math.floor(Math.random()*(max - min + 1))+min);
+
