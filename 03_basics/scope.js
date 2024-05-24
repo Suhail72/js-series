62a9daae67d713f34c9192a3873df02d3640bdbb
syed suhail <69636770+Suhail72@users.noreply.github.com>
2024-05-24 04:56:16 +0000

    scope

---
+++ 03_basics/scope.js
@@ -0,0 +1,41 @@
+function one() {
+    const userName = "suhail"
+    function two() {
+        const website = "youtube"
+       // console.log(userName);
+        
+        
+    } 
+    //console.log(website);
+    two()
+    }
+one()
+
+
+if(true){
+    const username = "samurai"
+    if (username==="samurai") {
+        const web = "google"
+        //console.log(username + web);
+        
+    }
+    //console.log(web);
+}
+//console.log(username);
+
+
+
+
+//console.log(addOne(5));
+function addOne(num) {
+    return num +1
+
+    
+}
+//console.log(addOne(5));
+
+//console.log(addtwo(8));
+const addtwo = function(num){
+    return num +2
+}
+//console.log(addtwo(8));
